0335973a9e5e39b9e3969c95d52efe12d3de3feb
andreiei <42600818+andreiei@users.noreply.github.com>
2024-12-28 19:53:28 +0100

    Update images

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './wow.jpg';
 import './App.css';
 
 const App = () => (
@@ -7,16 +7,8 @@ const App = () => (
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo"/>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+            "Clean your cup"
         </p>
-        <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-          Please do not learn React
-        </a>
       </header>
     </div>
 );

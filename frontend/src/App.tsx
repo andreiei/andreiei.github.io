e80a9222cfe2eb92c4906cd2351edb5c8a300d95
andreiei <42600818+andreiei@users.noreply.github.com>
2024-12-29 02:23:07 +0100

    Update styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,28 @@
-import React, { useState } from 'react';
-import Button from './Button';
+import React from 'react';
+import styled from 'styled-components';
 import logo from './wow.jpg';
 import './App.css';
 
-const App = (): React.ReactElement => {
-    const [showText, setShowText] = useState<boolean>(false);
+const H1 = styled.text`
+    font-size: 60px;
+    color: hotpink;
+    font-weight: bold;
+    font-family: Verdana;
+`;
 
+const H2 = styled.text`
+    font-size: 28px;
+    color: black;
+    font-family: 'Brush Script MT';
+`;
+
+const App = (): React.ReactElement => {
     return (
         <div className="App">
             <header className="App-header">
+                <H1>I SPREAD JOY</H1>
+                <H2>(in my own way)</H2>
                 <img src={logo} className="App-logo" alt="logo" />
-                {!showText ? (
-                    <Button onClick={(): void => setShowText(true)} title="Click" />
-                ) : (
-                    <div>&quot;I spread joy (in my own way)&quot;</div>
-                )}
             </header>
         </div>
     );

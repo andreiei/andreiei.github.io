0486220c49f7382f7dc84623cdbfcb6cf8e330d6
Andrei Eismont <andn_92@hotmail.com>
2024-12-29 20:40:27 +0100

    Update css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import styles from './App.module.scss';
-import logo from './wow.jpg';
+import img from './wow.jpg';
 
-const App = (): React.ReactElement => (
+const App: () => React.ReactElement = (): React.ReactElement => (
     <div className={styles.app}>
         <div className={styles.content}>
             <div className={styles.textWrapper}>
                 <div className={styles.h1}>I SPREAD JOY</div>
                 <div className={styles.h2}>(in my own way)</div>
             </div>
-            <img src={logo} className={styles.logo} alt="logo" />
+            <img src={img} className={styles.logo} alt="logo" />
         </div>
     </div>
 );

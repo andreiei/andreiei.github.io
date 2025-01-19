f091704d5b00cf5161a433ddcbffb7db02b6e5ed
Andrei Eismont <andn_92@hotmail.com>
2025-01-19 21:05:33 +0100

    Add mad and happy andrei

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './App.module.scss';
-import img from './wow.jpg';
+import happyAndrei from './happy_andrei.jpg';
+import madAndrei from './mad_andrei.jpg';
 
-const App: () => React.ReactElement = (): React.ReactElement => (
-    <div className={styles.app}>
-        <div className={styles.content}>
-            <div className={styles.textWrapper}>
-                <div className={styles.h1}>I SPREAD JOY</div>
-                <div className={styles.h2}>(in my own way)</div>
+const App: () => React.ReactElement = (): React.ReactElement => {
+    const [showHappy, setShowHappy] = useState<boolean>(true);
+    return (
+        <div className={styles.app}>
+            <div className={styles.content}>
+                <div className={styles.textWrapper}>
+                    <div className={styles.h1}>I SPREAD JOY</div>
+                    <div className={styles.h2}>(in my own way)</div>
+                </div>
+                <div
+                    role="button"
+                    onClick={(): void => setShowHappy(!showHappy)}
+                    onKeyUp={(): void => setShowHappy(!showHappy)}
+                    tabIndex={0}
+                    className={styles.logo}
+                >
+                    <img src={showHappy ? happyAndrei : madAndrei} className={styles.logo} alt="logo" />
+                </div>
             </div>
-            <img src={img} className={styles.logo} alt="logo" />
         </div>
-    </div>
-);
+    );
+};
 
 export default App;
